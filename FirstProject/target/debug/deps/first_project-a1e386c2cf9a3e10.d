b7edcb56539c85afb463dad9f5aae540612ec49e
Santiago Ramirez <sramzz@outlook.com>
2024-09-19 10:10:52 +0200

    removing targets again

--- FirstProject/target/debug/deps/first_project-a1e386c2cf9a3e10.d
+++
@@ -1,5 +0,0 @@
-/Users/sramzzs4d/Projects-sramzz/rustTraining/FirstProject/target/debug/deps/libfirst_project-a1e386c2cf9a3e10.rmeta: src/main.rs
-
-/Users/sramzzs4d/Projects-sramzz/rustTraining/FirstProject/target/debug/deps/first_project-a1e386c2cf9a3e10.d: src/main.rs
-
-src/main.rs:
